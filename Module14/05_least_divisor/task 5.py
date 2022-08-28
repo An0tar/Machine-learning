935e8767c09ed588aa83ee2a1a53f7d8da3d6b13
Artem Rozhkov <pdelivron@gmail.com>
2022-08-28 20:53:56 +0300

    'replace'

--- Module14/05_least_divisor/task 5.py
+++
@@ -1,6 +0,0 @@
-number = int(input('Введите число: '))
-min = number
-for num in range(number, 1, -1):
-    if number % num == 0:
-        min = num
-print('Наименьший делитель, отличный от единицы:', min)
--- Module15/task 9.py
+++
@@ -1,5 +0,0 @@
-word = list(input('Введите слово: '))
-if word == word[::-1]:
-    print('Да, является')
-else:
-    print('Нет, не является')
